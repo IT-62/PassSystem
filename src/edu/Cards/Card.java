7c34dbb0f3f32b7576a88ebd25f2f0af132e1b4d
ilyinalex <airbam@yandex>
2018-03-02 00:22:20 +0200

    Card.toString() fixed

--- src/edu/Cards/Card.java
+++ src/edu/Cards/Card.java
@@ -17,6 +17,7 @@ public abstract class Card {
     public String toString() {
         return "Card{" +
                 "id=" + id +
+                ", ownerType=" + ownerType +
                 '}';
     }
 }
