33d6a5bc47b1de78bb8eb2d9fffa6263fb528932
ilyinalex <airbam@yandex.ru>
2018-03-02 00:21:07 +0200

    superclass Card + first enum-type

--- src/edu/Cards/Card.java
+++ src/edu/Cards/Card.java
@@ -1,4 +1,22 @@
 package edu.Cards;
 
+import edu.options.OwnerType;
+
+import java.util.UUID;
+
 public abstract class Card {
+    private UUID id;
+    private OwnerType ownerType;
+    public UUID getID(){
+        return id;
+    }
+
+    public abstract boolean verify();
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "id=" + id +
+                '}';
+    }
 }
