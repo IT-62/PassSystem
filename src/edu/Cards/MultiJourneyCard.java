1cd4dd51cc3bc6e9b9e1a5d21504c047e18d4cfd
ilyinalex <airbam@yandex>
2018-03-02 00:36:35 +0200

    basic classes added.

---
+++ src/edu/Cards/MultiJourneyCard.java
@@ -0,0 +1,8 @@
+package edu.Cards;
+
+public class MultiJourneyCard extends Card {
+    @Override
+    public boolean verify() {
+        return false;
+    }
+}
---
+++ src/edu/Cards/TemporaryCard.java
@@ -0,0 +1,8 @@
+package edu.Cards;
+
+public class TemporaryCard extends Card {
+    @Override
+    public boolean verify() {
+        return false;
+    }
+}
