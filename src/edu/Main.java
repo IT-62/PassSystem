e9c804d564666c38b2a875e5d22ffdd9f734d83e
AlexStudent <sanchofeed@gmail.com>
2018-03-02 20:54:52 +0200

    Testing pull requests again

--- src/edu/Main.java
+++ src/edu/Main.java
@@ -2,7 +2,7 @@ package edu;
 
 public class Main {
     // testing pushing
-    // pull request test
+    // pull request test #2
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
