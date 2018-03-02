85013cf95abff2ef99edc6d39823da65d85d3f3c
AlexStudent <sanchofeed@gmail.com>
2018-03-02 20:35:38 +0200

    Added DurationType for cards and fixed TravelsCountType

--- src/edu/options/TravelsCountType.java
+++ src/edu/options/TravelsCountType.java
@@ -1,5 +1,16 @@
 package edu.options;
 
 public enum  TravelsCountType {
-    FIVEDAY, MONTHLY
+    FIVE(5),
+    TEN(10);
+
+    private int passesLeft;
+
+    TravelsCountType(int passesLeft) {
+        this.passesLeft = passesLeft;
+    }
+
+    public int getPassesLeft() {
+        return passesLeft;
+    }
 }
