ba5d54d1d46e52e017662b9530541e240fa27a20
AlexStudent <sanchofeed@gmail.com>
2018-03-02 20:50:21 +0200

    Testing pull requests in Intellij IDEA

--- src/edu/Main.java
+++ src/edu/Main.java
@@ -1,7 +1,8 @@
 package edu;
 
 public class Main {
-    //testing pushing
+    // testing pushing
+    // pull request test
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
