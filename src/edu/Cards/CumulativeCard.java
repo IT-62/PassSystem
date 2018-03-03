3ec92c86b37c263d7fd1c6fd58c665fea29ca817
ilyinalex <airbam@yandex>
2018-03-03 14:51:04 +0200

    Code review results. Cards and options are implemented.

--- src/edu/Cards/CumulativeCard.java
+++ src/edu/Cards/CumulativeCard.java
@@ -15,19 +15,21 @@ public class CumulativeCard extends Card {
     }
     //SetBalance provides extra freedom. I guess...
     public void replenish(int amount){
-        balance+=amount;
+        balance += amount;
     }
-    public boolean withdraw(){
+    public boolean withdraw() {
         if(balance >= travelPrice){
-            balance-=travelPrice;
+            balance -= travelPrice;
             return true;
         }
         return false;
     }
+    public int getBalance() {
+        return balance;
+    }
 
     @Override
     public boolean verify() {
-        if(withdraw()) return true;
-        return false;
+        return withdraw();
     }
 }
