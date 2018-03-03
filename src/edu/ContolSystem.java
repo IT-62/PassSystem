0ebf161af472c307ba71997983fb5dacc675f8c2
wowaolex <vovaolex123@gmail.com>
2018-03-03 15:39:57 +0200

    Initial Pass implementation and added method prototype to ControlSystem

--- src/edu/ContolSystem.java
+++ src/edu/ContolSystem.java
@@ -1,4 +1,15 @@
 package edu;
 
+import java.util.UUID;
+
 public class ContolSystem {
+    private ContolSystem() {
+
+    }
+
+    public String getCardInfo(UUID id) {
+        return "";
+    }
+
+
 }
